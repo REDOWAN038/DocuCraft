6f666a330a785abe40fa771a32d759c00bce79a8
REDOWAN038 <redowanibrahim2724@gmail.com>
2025-06-30 12:08:05 +0600

    initial setup, heaser and sidebar

---
+++ my-app/components/Logo.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Logo = () => {
+    return (
+        <div className="lg:flex">
+            <Link href="/">
+                <Image src="/logo.svg" alt="Logo" className="h-6 w-auto" width={100} height={24} priority />
+            </Link>
+        </div>
+
+    )
+}
+
+export default Logo
